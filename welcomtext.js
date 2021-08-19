37c0bd61ca4fda7fdef680c082e6e210563c710d
sergioTrmx <80510599+sergioTrmx@users.noreply.github.com>
2021-08-19 01:24:55 -0600

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,8 +1,20 @@
 const welcome = (number, groupname) => {
-    return `ola @${number}. 
-bienvenido al grupo🤙🏾
-${groupname}
-Recuerda Presentarte y leer las reglas :D `
+    return `*『Hola』* 
+
+*『@${number}. 』*
+
+*『Bienvenido al grupo』* 
+ Ꮠ ► ${groupname}
+
+𝙋𝙧𝙤𝙝𝙞𝙗𝙞𝙙𝙤 🚫
+Ꮠ ► 𝐍𝐨 𝐬𝐩𝐚𝐦 𝐲 𝐧𝐚𝐝𝐚 𝐝𝐞 𝐛𝐢𝐧𝐚𝐫𝐢𝐨𝐬 
+
+Ꮠ ► 𝐋𝐢𝐧𝐤 𝐝𝐞 𝐭𝐮 𝐠𝐫𝐮𝐩𝐨
+
+Ꮠ ► 𝐔𝐬𝐚𝐫 𝐞𝐥 𝐛𝐨𝐭 𝐩𝐚𝐫𝐚 𝐜𝐨𝐬𝐚𝐬 +18
+
+*『Disfruta.』* 
+http://ຯ❏͜ᰰ🍡۪۪ị¡-𝕾𝖝𝖗𝖌𝖝𝖝.liola `
 }
 exports.welcome = welcome
 
